a99233b0f2194900178133d86a1ed34fb78f2133
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-02-05 22:28:36 +0200

    Add task that sums fractions

---
+++ SumFraction/SumFraction/Fraction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumFraction
+{
+    internal class Fraction
+    {
+        public int Numerator { set; get; }
+        public int Denomirator { set; get; }    
+
+        public Fraction(int numerator, int denomirator)
+        {
+            if(denomirator == 0)
+            {
+                throw new Exception("Denominator cannot be Zero!");
+            }
+            Numerator = numerator;
+            Denomirator = denomirator;
+        }
+
+        private void Simplify()
+        {
+            int gcd = GCD(Numerator, Denomirator);
+            Numerator /= gcd;
+            Denomirator /= gcd;
+        }
+
+        private int GCD(int a, int b)
+        {
+            while(b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return Math.Abs(a);
+        }
+
+        public static Fraction Add(Fraction f1, Fraction f2)
+        {
+            int numerator = f1.Numerator * f2.Denomirator + f2.Numerator * f1.Denomirator;
+            int denominator = f1.Denomirator * f2.Denomirator;
+            return new Fraction(numerator, denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denomirator}";
+        }
+    }
+}
---
+++ SumFraction/SumFraction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumFraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the numerator and denominator of the first fraction (separated by space):");
+                var input1 = Console.ReadLine().Split(' ');
+                Fraction fraction1 = new Fraction(int.Parse(input1[0]), int.Parse(input1[1]));
+
+                Console.WriteLine("Enter the numerator and denominator of the second fraction (separated by space):");
+                var input2 = Console.ReadLine().Split(' ');
+                Fraction fraction2 = new Fraction(int.Parse(input2[0]), int.Parse(input2[1]));
+
+                Fraction result = Fraction.Add(fraction1, fraction2);
+
+                Console.WriteLine($"The sum of {fraction1} and {fraction2} is {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
