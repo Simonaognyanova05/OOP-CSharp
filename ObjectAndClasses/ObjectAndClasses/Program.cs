f5ecb979dbddd5ba77466677e81d760bf9e1c927
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-02-12 22:56:44 +0200

    Add task for Object and classes teme

---
+++ ObjectAndClasses/ObjectAndClasses/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Triangle[] triangles = new Triangle[3];
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                triangles[i] = new Triangle();
+
+                Console.WriteLine($"Въведете височината на триъгълник {i + 1}:");
+                triangles[i].Height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Въведете основата на триъгълник {i + 1}:");
+                triangles[i].Width = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < triangles.Length; i++)
+            {
+                Console.WriteLine($"Площта на триъгълник {i + 1} е: {triangles[i].GetArea():F2}");
+            }
+        }
+    }
+}
---
+++ ObjectAndClasses/ObjectAndClasses/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndClasses
+{
+    internal class Triangle
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+
+        public double GetArea()
+        {
+            return (Height * Width) / 2;  
+        }
+    }
+}
