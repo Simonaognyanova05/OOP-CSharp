79bbcfb124aef297c9bae476c0e5da1b62a8c2d6
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-02-19 19:19:31 +0200

    Add create contact function

--- ObjectAndClasses - 2/ObjectAndClasses - 2/Program.cs
+++ ObjectAndClasses - 2/ObjectAndClasses - 2/Program.cs
@@ -24,16 +24,16 @@ class Program
             switch (choice)
             {
                 case "1":
-                    AddContact(contacts);
+                    Add(contacts);
                     break;
                 case "2":
-                    SearchContact(contacts);
+                    //SearchContact(contacts);
                     break;
                 case "3":
-                    DeleteContact(contacts);
+                    //DeleteContact(contacts);
                     break;
                 case "4":
-                    DisplayAllContacts(contacts);
+                    //DisplayAllContacts(contacts);
                     break;
                 case "5":
                     isRunning = false;
@@ -46,73 +46,14 @@ class Program
         }
     }
 
-    static void AddContact(List<Contact> contacts)
+    static void Add(List<Contact> contacts)
     {
-        Console.Write("Enter Name: ");
         string name = Console.ReadLine();
-        Console.Write("Enter Phone Number: ");
         string phoneNumber = Console.ReadLine();
-        Console.Write("Enter Email: ");
-        string email = Console.ReadLine();
+        string email = Console.ReadLine();  
 
         contacts.Add(new Contact { Name = name, PhoneNumber = phoneNumber, Email = email });
-        Console.WriteLine("Contact added successfully.");
+        Console.WriteLine("Contact was added successfully!");
     }
-
-    static void SearchContact(List<Contact> contacts)
-    {
-        Console.Write("Enter Name or Phone Number to search: ");
-        string query = Console.ReadLine();
-
-        var results = contacts.FindAll(contact =>
-            contact.Name.Contains(query) ||
-            contact.PhoneNumber.Contains(query));
-
-        if (results.Count > 0)
-        {
-            Console.WriteLine("Search results:");
-            foreach (var contact in results)
-            {
-                Console.WriteLine(contact);
-            }
-        }
-        else
-        {
-            Console.WriteLine("No contacts found.");
-        }
-    }
-
-    static void DeleteContact(List<Contact> contacts)
-    {
-        Console.Write("Enter Phone Number to delete: ");
-        string phoneNumber = Console.ReadLine();
-
-        var contactToRemove = contacts.Find(contact => contact.PhoneNumber == phoneNumber);
-
-        if (contactToRemove != null)
-        {
-            contacts.Remove(contactToRemove);
-            Console.WriteLine("Contact deleted successfully.");
-        }
-        else
-        {
-            Console.WriteLine("Contact not found.");
-        }
-    }
-
-    static void DisplayAllContacts(List<Contact> contacts)
-    {
-        if (contacts.Count > 0)
-        {
-            Console.WriteLine("All contacts:");
-            foreach (var contact in contacts)
-            {
-                Console.WriteLine(contact);
-            }
-        }
-        else
-        {
-            Console.WriteLine("No contacts available.");
-        }
-    }
+    
 }
