9c365264060315b7591683a65765736745975772
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-05 23:50:39 +0200

    Add task 2 - drawing rectangle with class

---
+++ Inheritance/Task.2/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._2
+{
+    internal class Rectangle
+    {
+        public Point Position {  get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Paint(Graphics g)
+        {
+            Pen pen = new Pen(Color.Blue, 2);
+            g.DrawRectangle(pen, Position.X, Position.Y, Width, Height);
+        }
+    }
+}
