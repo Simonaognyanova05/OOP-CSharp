9dd4255c8b3c4b0517aca922373391af05f61b70
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-09 13:17:41 +0200

    Add new design to main form

--- CourseProject/CourseProject/MainForm.Designer.cs
+++ CourseProject/CourseProject/MainForm.Designer.cs
@@ -47,187 +47,218 @@
             this.lblY = new System.Windows.Forms.Label();
             this.lblX = new System.Windows.Forms.Label();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblType
             // 
             this.lblType.AutoSize = true;
-            this.lblType.Location = new System.Drawing.Point(30, 35);
+            this.lblType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblType.Location = new System.Drawing.Point(50, 102);
             this.lblType.Name = "lblType";
-            this.lblType.Size = new System.Drawing.Size(109, 20);
+            this.lblType.Size = new System.Drawing.Size(165, 25);
             this.lblType.TabIndex = 0;
-            this.lblType.Text = "Type of figure:";
+            this.lblType.Text = "Enter shape type:";
             // 
             // lblA
             // 
             this.lblA.AutoSize = true;
-            this.lblA.Location = new System.Drawing.Point(106, 164);
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(37, 189);
             this.lblA.Name = "lblA";
-            this.lblA.Size = new System.Drawing.Size(24, 20);
+            this.lblA.Size = new System.Drawing.Size(178, 25);
             this.lblA.TabIndex = 1;
-            this.lblA.Text = "A:";
+            this.lblA.Text = "Enter a value for A:";
             this.lblA.Visible = false;
             // 
             // lblB
             // 
             this.lblB.AutoSize = true;
-            this.lblB.Location = new System.Drawing.Point(106, 219);
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblB.Location = new System.Drawing.Point(37, 235);
             this.lblB.Name = "lblB";
-            this.lblB.Size = new System.Drawing.Size(24, 20);
+            this.lblB.Size = new System.Drawing.Size(177, 25);
             this.lblB.TabIndex = 2;
-            this.lblB.Text = "B:";
+            this.lblB.Text = "Enter a value for B:";
             this.lblB.Visible = false;
             // 
             // txtType
             // 
-            this.txtType.Location = new System.Drawing.Point(145, 35);
+            this.txtType.Location = new System.Drawing.Point(232, 103);
             this.txtType.Name = "txtType";
-            this.txtType.Size = new System.Drawing.Size(202, 26);
+            this.txtType.Size = new System.Drawing.Size(287, 26);
             this.txtType.TabIndex = 3;
             // 
             // txtA
             // 
-            this.txtA.Location = new System.Drawing.Point(145, 164);
+            this.txtA.Location = new System.Drawing.Point(232, 188);
             this.txtA.Name = "txtA";
-            this.txtA.Size = new System.Drawing.Size(202, 26);
+            this.txtA.Size = new System.Drawing.Size(287, 26);
             this.txtA.TabIndex = 4;
             this.txtA.Visible = false;
             // 
             // txtB
             // 
-            this.txtB.Location = new System.Drawing.Point(145, 206);
+            this.txtB.Location = new System.Drawing.Point(232, 234);
             this.txtB.Name = "txtB";
-            this.txtB.Size = new System.Drawing.Size(202, 26);
+            this.txtB.Size = new System.Drawing.Size(287, 26);
             this.txtB.TabIndex = 5;
             this.txtB.Visible = false;
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(224, 350);
+            this.btnAdd.BackColor = System.Drawing.SystemColors.Info;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(396, 374);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(123, 41);
             this.btnAdd.TabIndex = 6;
             this.btnAdd.Text = "Add figure";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.UseVisualStyleBackColor = false;
             this.btnAdd.Visible = false;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // lblFigures
             // 
             this.lblFigures.AutoSize = true;
-            this.lblFigures.Location = new System.Drawing.Point(383, 35);
+            this.lblFigures.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFigures.Location = new System.Drawing.Point(549, 92);
             this.lblFigures.Name = "lblFigures";
-            this.lblFigures.Size = new System.Drawing.Size(66, 20);
+            this.lblFigures.Size = new System.Drawing.Size(544, 25);
             this.lblFigures.TabIndex = 7;
-            this.lblFigures.Text = "Figures:";
+            this.lblFigures.Text = "Select a shape to calculate the face, draw it, or erase it.";
             this.lblFigures.Click += new System.EventHandler(this.label1_Click);
             // 
             // lstFigures
             // 
             this.lstFigures.FormattingEnabled = true;
             this.lstFigures.ItemHeight = 20;
-            this.lstFigures.Location = new System.Drawing.Point(455, 35);
+            this.lstFigures.Location = new System.Drawing.Point(564, 135);
             this.lstFigures.Name = "lstFigures";
-            this.lstFigures.Size = new System.Drawing.Size(386, 184);
+            this.lstFigures.Size = new System.Drawing.Size(539, 184);
             this.lstFigures.TabIndex = 8;
             // 
             // txtArea
             // 
-            this.txtArea.Location = new System.Drawing.Point(455, 238);
+            this.txtArea.Location = new System.Drawing.Point(625, 334);
             this.txtArea.Name = "txtArea";
-            this.txtArea.Size = new System.Drawing.Size(386, 26);
+            this.txtArea.Size = new System.Drawing.Size(478, 26);
             this.txtArea.TabIndex = 11;
             // 
             // lblArea
             // 
             this.lblArea.AutoSize = true;
-            this.lblArea.Location = new System.Drawing.Point(402, 238);
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(559, 332);
             this.lblArea.Name = "lblArea";
-            this.lblArea.Size = new System.Drawing.Size(47, 20);
+            this.lblArea.Size = new System.Drawing.Size(60, 25);
             this.lblArea.TabIndex = 12;
             this.lblArea.Text = "Area:";
             // 
             // btnCalcArea
             // 
-            this.btnCalcArea.Location = new System.Drawing.Point(589, 286);
+            this.btnCalcArea.BackColor = System.Drawing.SystemColors.Info;
+            this.btnCalcArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcArea.Location = new System.Drawing.Point(731, 374);
             this.btnCalcArea.Name = "btnCalcArea";
-            this.btnCalcArea.Size = new System.Drawing.Size(123, 41);
+            this.btnCalcArea.Size = new System.Drawing.Size(210, 41);
             this.btnCalcArea.TabIndex = 13;
-            this.btnCalcArea.Text = "Calc Area";
-            this.btnCalcArea.UseVisualStyleBackColor = true;
+            this.btnCalcArea.Text = "Calculate Area";
+            this.btnCalcArea.UseVisualStyleBackColor = false;
             this.btnCalcArea.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(215, 67);
+            this.btnCheck.BackColor = System.Drawing.SystemColors.Info;
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(317, 135);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(132, 63);
+            this.btnCheck.Size = new System.Drawing.Size(202, 45);
             this.btnCheck.TabIndex = 14;
             this.btnCheck.Text = "Check your figure";
-            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.UseVisualStyleBackColor = false;
             this.btnCheck.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnDraw
             // 
-            this.btnDraw.Location = new System.Drawing.Point(460, 286);
+            this.btnDraw.BackColor = System.Drawing.SystemColors.Info;
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDraw.Location = new System.Drawing.Point(564, 374);
             this.btnDraw.Name = "btnDraw";
-            this.btnDraw.Size = new System.Drawing.Size(123, 41);
+            this.btnDraw.Size = new System.Drawing.Size(161, 41);
             this.btnDraw.TabIndex = 15;
             this.btnDraw.Text = "Draw figure";
-            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.UseVisualStyleBackColor = false;
             this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
             // 
             // txtY
             // 
-            this.txtY.Location = new System.Drawing.Point(145, 304);
+            this.txtY.Location = new System.Drawing.Point(232, 342);
             this.txtY.Name = "txtY";
-            this.txtY.Size = new System.Drawing.Size(202, 26);
+            this.txtY.Size = new System.Drawing.Size(287, 26);
             this.txtY.TabIndex = 20;
             this.txtY.Visible = false;
             // 
             // txtX
             // 
-            this.txtX.Location = new System.Drawing.Point(145, 262);
+            this.txtX.Location = new System.Drawing.Point(232, 286);
             this.txtX.Name = "txtX";
-            this.txtX.Size = new System.Drawing.Size(202, 26);
+            this.txtX.Size = new System.Drawing.Size(287, 26);
             this.txtX.TabIndex = 19;
             this.txtX.Visible = false;
             // 
             // lblY
             // 
             this.lblY.AutoSize = true;
-            this.lblY.Location = new System.Drawing.Point(106, 310);
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblY.Location = new System.Drawing.Point(20, 343);
             this.lblY.Name = "lblY";
-            this.lblY.Size = new System.Drawing.Size(24, 20);
+            this.lblY.Size = new System.Drawing.Size(194, 25);
             this.lblY.TabIndex = 18;
-            this.lblY.Text = "Y:";
+            this.lblY.Text = "Enter a coordinate Y:";
             this.lblY.Visible = false;
             // 
             // lblX
             // 
             this.lblX.AutoSize = true;
-            this.lblX.Location = new System.Drawing.Point(106, 262);
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(20, 287);
             this.lblX.Name = "lblX";
-            this.lblX.Size = new System.Drawing.Size(24, 20);
+            this.lblX.Size = new System.Drawing.Size(195, 25);
             this.lblX.TabIndex = 17;
-            this.lblX.Text = "X:";
+            this.lblX.Text = "Enter a coordinate X:";
             this.lblX.Visible = false;
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(718, 286);
+            this.btnDelete.BackColor = System.Drawing.SystemColors.Info;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(947, 374);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(123, 41);
+            this.btnDelete.Size = new System.Drawing.Size(156, 41);
             this.btnDelete.TabIndex = 21;
             this.btnDelete.Text = "Delete figure";
-            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(478, 31);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(247, 32);
+            this.lblTitle.TabIndex = 22;
+            this.lblTitle.Text = "Graphical figures";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 419);
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1190, 457);
+            this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.txtY);
             this.Controls.Add(this.txtX);
@@ -247,7 +278,9 @@
             this.Controls.Add(this.lblB);
             this.Controls.Add(this.lblA);
             this.Controls.Add(this.lblType);
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
@@ -276,6 +309,7 @@
         private System.Windows.Forms.Label lblY;
         private System.Windows.Forms.Label lblX;
         private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
