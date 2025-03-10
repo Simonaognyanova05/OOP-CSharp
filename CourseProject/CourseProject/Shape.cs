28644de229a630fed3c4db5b2ad3f5a5374f1749
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-10 23:06:46 +0200

    Add new class Rectangle

--- CourseProject/CourseProject/Shape.cs
+++ CourseProject/CourseProject/Shape.cs
@@ -27,33 +27,6 @@ namespace CourseProject
             return $"at ({X}, {Y})";
          }
     }
-
-    public class Rectangle : Shape
-    {
-        public int Width { get; set; }
-        public int Height { get; set; }
-
-        public Rectangle(int x, int y, int width, int height) : base(x, y)
-        {
-            Width = width;
-            Height = height;
-        }
-
-        public override double CalculateArea()
-        {
-            return Width * Height;
-        }
-
-        public override void Draw(Graphics g)
-        {
-            Pen pen = new Pen(Color, 2);
-            g.DrawRectangle(pen, X, Y, Width, Height);
-        }
-        public override string ToString()
-        {
-            return $"Rectangle: Width = {Width}; Height = {Height}";
-        }
-    }
     public class Circle : Shape
     {
         public int Radius { get; set; }
