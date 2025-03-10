97392d9cd0efcf5b9c5d2e41ef237dbb730020dd
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-10 23:04:35 +0200

    Add new class Square

--- CourseProject/CourseProject/Shape.cs
+++ CourseProject/CourseProject/Shape.cs
@@ -28,30 +28,6 @@ namespace CourseProject
          }
     }
 
-    public class Square : Shape
-    {
-        public int A { get; set; }
-
-        public Square(int x, int y, int a) : base(x, y)
-        {
-            A = a;
-        }
-
-        public override double CalculateArea()
-        {
-            return A * A;
-        }
-
-        public override void Draw(Graphics g)
-        {
-            Pen pen = new Pen(Color, 2);
-            g.DrawRectangle(pen, X, Y, A, A);
-        }
-        public override string ToString()
-        {
-            return $"Square: Side = {A}";
-        }
-    }
     public class Rectangle : Shape
     {
         public int Width { get; set; }
---
+++ CourseProject/CourseProject/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public class Square : Shape
+    {
+        public int A { get; set; }
+
+        public Square(int x, int y, int a) : base(x, y)
+        {
+            A = a;
+        }
+
+        public override double CalculateArea()
+        {
+            return A * A;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+            g.DrawRectangle(pen, X, Y, A, A);
+        }
+        public override string ToString()
+        {
+            return $"Square: Side = {A}";
+        }
+    }
+}
