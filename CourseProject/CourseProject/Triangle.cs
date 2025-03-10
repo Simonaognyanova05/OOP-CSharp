c6f3041c12503623d4e855c40e3fd2bd1507e0b0
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-03-10 23:12:53 +0200

    Add new class Triangle

---
+++ CourseProject/CourseProject/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    public class Triangle : Shape
+    {
+        public int BaseLength { get; set; }
+        public int Height { get; set; }
+
+        public Triangle(int x, int y, int baseLength, int height) : base(x, y)
+        {
+            BaseLength = baseLength;
+            Height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * BaseLength * Height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+
+            Point[] points = {
+                new Point(X, Y),
+                new Point(X + BaseLength / 2, Y - Height),
+                new Point(X - BaseLength / 2, Y - Height)
+            };
+
+            g.DrawPolygon(pen, points);
+        }
+
+        public override string ToString()
+        {
+            return $"Triangle: Side = {BaseLength}; Height = {Height}";
+        }
+    }
+}
